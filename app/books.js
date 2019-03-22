469842bc1dcbf56a9379d731c953ec2d838e6847
Gillian Openshaw <gillianopenshaw@macusers-iMac.local>
2019-03-22 11:35:17 +0000

    1st Attempt

---
+++ app/books.js
@@ -0,0 +1,15 @@
+class Book {
+    constructor(title, author, ISBN) {
+      this.title = title;
+      this.author = author;
+      this.available = true;
+      this.ISBN = ISBN;
+  
+    }
+
+    add() {
+    }
+}
+
+
+    module.exports = Book;
